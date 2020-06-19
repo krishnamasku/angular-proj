29c1cc173fdb77011543934ae00b217cc9ae3840
Krishna Masuku <kmasuku@CSC-US-RSR-M001.COM>
2020-06-19 01:38:50 -0400

    changes made to home component

--- src/app/components/phone-directory-home/phone-directory-home.component.ts
+++ src/app/components/phone-directory-home/phone-directory-home.component.ts
@@ -10,11 +10,23 @@ import { NavigationService } from 'src/app/services/navigation.service';
 })
 export class PhoneDirectoryHomeComponent {
 
+  digitTenWarn: boolean = false;
+
   phoneDirForm = new FormGroup({
     phoneNumber: new FormControl(null, [Validators.required, phoneNumberLengthValidator])
   })
 
-  constructor(private _navigation:NavigationService) { }
+  constructor(private _navigation: NavigationService) { }
+
+  onBlur(phoneNumber: string) {
+    const regex: RegExp = new RegExp('^[1-9][0-9]{9}$')
+    if (phoneNumber) {
+      if ((phoneNumber.length === 10 && 
+              !String(phoneNumber).match(regex))) {
+        this.digitTenWarn = true;
+      }
+    }
+  }
 
   onSubmit() {
     this._navigation.getPhoneDataPage(this.phoneDirForm);
@@ -28,10 +40,10 @@ function phoneNumberLengthValidator(control: AbstractControl): { [key: string]: 
   const regex2: RegExp = new RegExp('^[0-9]{7}$')
 
   if (String(phoneNumber).match(regex1) || String(phoneNumber).match(regex2)) {
-      return null
-  }else if(phoneNumber && phoneNumber.length>6){
+    return null
+  } else if (phoneNumber && phoneNumber.length > 6) {
     return { 'phoneNumberLengthValidator': true }
-  }else{
+  } else {
     return { 'phoneNumberLengthValidator': true }
   }
 }
